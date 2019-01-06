1eadaba0ea9c399d84a88b4bb19983ad703a0e7b
hwsxsf <haowensxsf@163.com>
2019-01-06 14:58:40 +0800

    struts labels

---
+++ src/main/java/com/xyd/action/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.xyd.action;
+
+public class HelloWorld {
+		
+		public String hello(){
+			System.out.println("调用action。。。");
+			return "ok";
+		}
+		
+		//通配符
+		public String hi(){
+			System.out.println("调用action。。。123231");
+			return "ok";
+		}
+		
+
+}
